92196f63ab52e8935dec5e43c708585df77e195c
Administrator <mc96107@gmail.com>
2014-03-08 23:49:43 +0200

    Signed-off-by: Administrator <mc96107@gmail.com>

--- app.js
+++ app.js
@@ -150,3 +150,16 @@ cfile=nfl;
 }
 }
 }
+
+//setclli(list.childNodes[0].childNodes,'isDir current');
+//setclli(list.childNodes[0].childNodes,'isDir');
+
+function setclli(p,class){
+var t=p.childNodes;
+for (var i = 0; i < t.length; i++) {
+if(t[i].className!='isnotDir'){
+t[i].className=class;
+setclli(t[i],class);
+}
+}
+}
