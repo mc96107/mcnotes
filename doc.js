c01d969dc22ae3d2c423c6193e8d7cb2481ba9ad
Administrator <mc96107@gmail.com>
2014-04-07 01:39:35 +0300

    fix delete

--- doc.js
+++ doc.js
@@ -78,7 +78,7 @@ privateClient.declareType('index', {
 	}
 	else remoteStorage.mcnotes.readFile(f);
 	},
-	removeFile: function(f){if (f.substring(f.length-3,f.length)!='.md') f=f+'.md';synctmp=1;privateClient.remove(f).then(function(){syncrefreshlista();});},
+	removeFile: function(f){privateClient.remove(f).then(function(){if(!synctmp) var timeoutID = window.setTimeout(function(){remoteStorage.mcnotes.removeFile(f);synctmp=1;syncrefreshlista();}, 5000);});},
     	mf: function(p1,p2){privateClient.getFile(p1).then(function(file){
     	//privateClient.storeFile('text/plain', p2, file.data);
     	remoteStorage.mcnotes.writeFile(p2,file.data);
